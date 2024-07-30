f70cc1247f0bff1cb72e8418d9bf58b69590c429
operaciones2022 <sistemas.odisec@gmail.com>
2024-07-30 15:03:57 -0500

    despliegue

--- server.js
+++ server.js
@@ -3,25 +3,26 @@ const cors = require('cors');
 const router = require('./src/routes/index');
 const { sequelize } = require('./src/config/bdCon');
 
-//modelos
+// modelos
 require('./src/models/Derivaciones');
 require('./src/models/JZ.model');
 require('./src/models/Supervisor.model');
 require('./src/models/Agencia.model');
-require('./src/models/helpers/ModelHelper')
-//finModelos
+require('./src/models/helpers/ModelHelper');
+// finModelos
 
 const app = express();
-const { PORT } = process.env;
-const port = PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
-sequelize.sync().then(()=>{
-  console.log(`MOdelos Sincronizados con la base de datos`)
-  app.listen(port,()=>{
-    console.log(`servidor https://localhost:${port} `);
-  })
-}).catch((err)=>{console.log(err)})
+sequelize.sync().then(() => {
+  console.log('Modelos Sincronizados con la base de datos');
+  app.listen(port, () => {
+    console.log(`servidor escuchando en http://localhost:${port}`);
+  });
+}).catch((err) => {
+  console.log(err);
+});
