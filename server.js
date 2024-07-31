b1b6766a11cc09563497457203647f963ac77fdd
operaciones2022 <sistemas.odisec@gmail.com>
2024-07-31 11:28:40 -0500

    correccion de la ruta de bdcon y config deploy 7

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./src/routes/index');
-const { sequelize } = require('./src/config/bdCon');
+const {sequelize} = require('./src/config/bdCon')
 
-// modelos
+// Modelos
 require('./src/models/Derivaciones');
 require('./src/models/JZ.model');
 require('./src/models/Supervisor.model');
 require('./src/models/Agencia.model');
 require('./src/models/helpers/ModelHelper');
-// finModelos
+// Fin de modelos
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,16 +18,19 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
+// Middleware para manejo de errores
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error('Error en el servidor:', err);
   res.status(500).send('Something broke!');
 });
 
-sequelize.sync().then(() => {
-  console.log('Modelos Sincronizados con la base de datos');
-  app.listen(port, () => {
-    console.log(`servidor escuchando en http://localhost:${port}`);
+sequelize.sync()
+  .then(() => {
+    console.log('Modelos Sincronizados con la base de datos');
+    app.listen(port, () => {
+      console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error al sincronizar modelos:', err);
   });
-}).catch((err) => {
-  console.log(err);
-});
