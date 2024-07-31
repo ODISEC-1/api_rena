d33410f944e04ae68157fee86a1938699558b158
operaciones2022 <sistemas.odisec@gmail.com>
2024-07-31 10:57:19 -0500

    configuraciones

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 sequelize.sync().then(() => {
   console.log('Modelos Sincronizados con la base de datos');
   app.listen(port, () => {
